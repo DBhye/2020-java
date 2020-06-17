4174e68c642b2174406bf7fe806c7c19677cd17c
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-17 17:50:52 +0900

    homework

---
+++ first/src/com/first/mission/GreatMission3.java
@@ -0,0 +1,24 @@
+package com.first.mission;
+
+public class GreatMission3 {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		int [][] arr = {
+				{90, 100, 88 }, //영어
+				{34,99,45}, //수학
+				{98, 65, 87} //국어
+		};
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int z=0; z< arr[i].length; z++) {
+				
+				
+				}
+					
+					 
+				
+			}
+		} 
+	}
+
