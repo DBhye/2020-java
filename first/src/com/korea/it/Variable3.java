9da6f0e2648f125e14fbe1874908e41ac759d399
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-06-17 14:36:14 +0900

    야구게임

--- first/src/com/korea/it/Variable3.java
+++ first/src/com/korea/it/Variable3.java
@@ -5,13 +5,13 @@ public class Variable3 {
 	public static void main(String[] args) {
 		int n1 = 2_147_483_647;
 		long n4 = 222222;
-		int n1 = 10;
+		int n3 = 10;
 		float n2 = 50.2f;
 		
 	System.out.println(n1);
 	System.out.println(n2);
 	float result = n2 + n1;
-	double n3 = n2;
+	double n5 = n2;
 	
 	// float + int 결과값은 float형이다. float형이 우선순위가 더 높다.
 	}
